7a20629428e772fe5dccfe4a060e8a25b15f8be2
TwitoCode <toheebeji@gmail.com>
2022-06-02 07:22:29 -0400

    Added the Navbvar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,21 +1,35 @@
 <nav>
-  <div class="start">Hello</div>
+  <div class="start"><a href="/">Svelte Todos</a></div>
   <div class="middle" />
-  <div class="end" />
+  <div class="end">End</div>
 </nav>
 
 <style lang="scss">
   nav {
     display: flex;
     justify-content: space-between;
-    background-color: lightblue;
-    width: 100vw;
+    background-color: blueviolet;
+    max-width: 100vw;
+    padding: 20px 10rem;
+    color: white;
 
     .start {
+      flex: 1;
+      
+      a {
+        color: white;
+        text-decoration: none;
+      }
     }
     .middle {
+      display: flex;
+      justify-content: center;
+      flex: 3;
     }
     .end {
+      flex: 1;
+      display: flex;
+      justify-content: end;
     }
   }
 </style>
